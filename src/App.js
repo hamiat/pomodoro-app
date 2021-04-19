c06f814c72b81c21958c7ff6ce0763b5bab48c2d
hamiat <hamiat.n@hotmail.com>
2021-04-19 16:28:39 +0200

    formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Testings from './Testings'
+import "./App.css";
+import Pomodoro from "./Pomodoro";
 
 function App() {
   return (
     <div className="App">
-     <Testings />
+      <Pomodoro />
     </div>
   );
 }
